80a838142e5dbaab71fb2a7ed4f959053f7da551
flwfdd <1525876733@qq.com>
2023-09-04 21:18:05 +0800

    增加题库 修改失效图片链接

--- code/config.py
+++ code/config.py
@@ -1,7 +1,7 @@
 '''
 Author: flwfdd
 Date: 2022-08-28 16:40:56
-LastEditTime: 2022-09-07 15:28:07
+LastEditTime: 2023-09-04 21:15:58
 Description: 
 _(:з」∠)_
 '''
@@ -9,5 +9,5 @@ import os
 
 db_url="" or os.environ.get("db_url")
 
-geetest_id="" or os.environ.get("captcha_id")
-geetest_key="" or os.environ.get("captcha_key")
+geetest_id="" or os.environ.get("geetest_id")
+geetest_key="" or os.environ.get("geetest_key")
